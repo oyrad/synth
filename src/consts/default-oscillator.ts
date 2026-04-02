2505c9efa9065140094468e5573c3a3cca25da1c
Dario Susanj <dario.susanj@tradelocker.com>
2026-04-02 09:26:36 +0200

    fix: remove default oscillator

--- src/consts/default-oscillator.ts
+++
@@ -1,9 +0,0 @@
-import type { OscillatorParameters } from '../components/synth/oscillators.tsx';
-
-export const DEFAULT_OSCILLATOR_PARAMETERS: Omit<OscillatorParameters, 'id'> = {
-  waveform: 'sine',
-  isMute: false,
-  volume: 50,
-  detune: 0,
-  transpose: 0,
-};
