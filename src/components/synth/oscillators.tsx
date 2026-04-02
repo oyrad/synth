5c6871f9ddfb40ba7377e6aa9e958425c3a164e4
Dario Susanj <dario.susanj@tradelocker.com>
2026-04-02 11:44:27 +0200

    fix: oscillator values and colors

--- src/components/synth/oscillators.tsx
+++ src/components/synth/oscillators.tsx
@@ -29,36 +29,35 @@ export function Oscillators() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-4">
-      {oscillators.map((oscillator, index) => (
+      {oscillators.map(({ id, isActive, waveform, volume, detune, transpose }, index) => (
         <Card
+          key={id}
+          isActive={isActive}
           className={cn(
             index === 0 &&
-              'bg-blue-600/50 border-blue-600/80 dark:bg-blue-600/30 dark:border-blue-600/60',
+              'bg-blue-600/20 border-blue-600/80 dark:bg-blue-600/30 dark:border-blue-600/60',
             index === 1 &&
-              'bg-green-600/50 border-green-600/80 dark:bg-green-600/30 dark:border-green-600/60',
+              'bg-green-600/20 border-green-600/80 dark:bg-green-600/30 dark:border-green-600/60',
             index === 2 &&
-              'bg-rose-600/50 border-rose-600/80 dark:bg-rose-600/30 dark:border-rose-600/60',
+              'bg-rose-600/20 border-rose-600/80 dark:bg-rose-600/30 dark:border-rose-600/60',
             index === 3 &&
-              'bg-yellow-600/50 border-yellow-600/80 dark:bg-yellow-600/30 dark:border-yellow-600/60',
-            !oscillator.isActive &&
-              'opacity-50 pointer-events-none border-neutral-600/50 dark:border-neutral-600/60 bg-neutral-700/20 dark:bg-neutral-700/30',
+              'bg-yellow-600/20 border-yellow-600/80 dark:bg-yellow-600/30 dark:border-yellow-600/60',
           )}
-          key={oscillator.id}
         >
           <div className="flex justify-between items-center">
             <Title>oscillator {index + 1}</Title>
             <Switch
               className="pointer-events-auto"
-              checked={oscillator.isActive}
-              onCheckedChange={(checked) => updateOscillator(oscillator.id, { isActive: checked })}
+              checked={isActive}
+              onCheckedChange={(checked) => updateOscillator(id, { isActive: checked })}
             />
           </div>
 
           <Select
-            value={oscillator.waveform}
+            value={waveform}
             onValueChange={(value) => {
               if (isOscillatorType(value)) {
-                updateOscillator(oscillator.id, { waveform: value });
+                updateOscillator(id, { waveform: value });
               }
             }}
           >
@@ -77,29 +76,32 @@ export function Oscillators() {
 
           <SliderParam
             labelLeft="Volume"
-            value={oscillator.volume}
+            labelRight={`${volume}%`}
+            value={volume}
             min={0}
             max={100}
             step={1}
-            onChange={(value) => updateOscillator(oscillator.id, { volume: value[0] })}
+            onChange={(value) => updateOscillator(id, { volume: value[0] })}
           />
 
           <SliderParam
             labelLeft="Detune"
-            value={oscillator.detune}
+            labelRight={`${detune} cents`}
+            value={detune}
             min={-64}
             max={64}
             step={1}
-            onChange={(value) => updateOscillator(oscillator.id, { detune: value[0] })}
+            onChange={(value) => updateOscillator(id, { detune: value[0] })}
           />
 
           <SliderParam
             labelLeft="Transpose"
-            value={oscillator.transpose}
+            labelRight={`${transpose} semitones`}
+            value={transpose}
             min={-24}
             max={24}
             step={1}
-            onChange={(value) => updateOscillator(oscillator.id, { transpose: value[0] })}
+            onChange={(value) => updateOscillator(id, { transpose: value[0] })}
           />
         </Card>
       ))}
